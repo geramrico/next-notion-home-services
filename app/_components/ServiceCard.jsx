bf8b9eddd882c744e40618afe0e390417c00eb23
Gerardo <geramoralesrico@gmail.com>
2023-09-14 22:02:07 -0600

    improved layout of service card and dashboard

--- app/_components/ServiceCard.jsx
+++ app/_components/ServiceCard.jsx
@@ -18,37 +18,39 @@ function ServiceCard({ service }) {
   console.log(service)
   return (
     <Card key={service.id} className="flex flex-col h-full">
-      <CardHeader className="flex-grow">
-        <CardTitle>{service.name}</CardTitle>
-        <CardDescription>
-          <a
-            href={`tel:${service.phone}`}
-            target="_blank"
-            className="hover:underline"
-          >
-            {service.phone}
-          </a>
-        </CardDescription>
-      </CardHeader>
+      <div className="flex flex-col md:flex-row">
+        <CardHeader className="flex-grow">
+          <CardTitle>{service.name}</CardTitle>
+          <CardDescription className="flex flex-col">
+            <a
+              href={`tel:${service.phone}`}
+              target="_blank"
+              className="hover:underline hover:text-teal-600 focus:underline focus:text-teal-600"
+            >
+              {service.phone}
+            </a>
+            <p>{service.description}</p>
+          </CardDescription>
+        </CardHeader>
 
-      <CardContent className="flex-grow">
-        <div className="flex flex-wrap mt-2 mb-2">
-          {service.categories.map((category, index) => (
-            <Badge variant="secondary" key={index}>
-              {category}
-            </Badge>
-          ))}
-        </div>
+        <CardContent className="flex-grow flex-col align-middle">
+          <div className="flex flex-wrap mt-2 mb-2 justify-start">
+            {service.categories.map((category, index) => (
+              <Badge variant="secondary" key={index} className="ml-2">
+                {category}
+              </Badge>
+            ))}
+          </div>
+        </CardContent>
 
-        <p>{service.description}</p>
-      </CardContent>
-      <CardFooter className="mt-auto">
-        <Button className="w-full">
-          <Link href={`https://api.whatsapp.com/send?phone=${service.phone}`}>
-            Envía un mensaje
-          </Link>
-        </Button>
-      </CardFooter>
+        <CardFooter className="sm:pt-6">
+          <Button className="w-full hover:bg-teal-600 focus:bg-teal-600">
+            <Link href={`https://api.whatsapp.com/send?phone=${service.phone}`}>
+              Envía un mensaje
+            </Link>
+          </Button>
+        </CardFooter>
+      </div>
     </Card>
   )
 }
--- app/_components/ServicesDashboard.jsx
+++ app/_components/ServicesDashboard.jsx
@@ -29,7 +29,7 @@ const ServicesDashboard = ({ services }) => {
   console.log(allCategories)
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 gap-4">
       {services.map((service) => (
         <ServiceCard service={service} />
       ))}
