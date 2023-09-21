c3a2ecf9f0d94a080a03288a064ff094d3a511a7
Gerardo <geramoralesrico@gmail.com>
2023-09-21 13:23:44 -0600

    added vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 import { SiteFooter } from '@/components/site-footer'
 import Nav from './_components/Nav'
-
-
 
 export const metadata = {
   title: 'Servicios Saltillo',
@@ -13,11 +12,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <Nav/>
+        <Nav />
         <div className="relative flex min-h-screen flex-col">
           <div className="flex-1">{children}</div>
         </div>
         <SiteFooter />
+        <Analytics />
       </body>
     </html>
   )
