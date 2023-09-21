8a657c880bc9f6875a8af4ea42fa1d51453272ad
Gerardo <geramoralesrico@gmail.com>
2023-09-21 13:12:23 -0600

    deleted accents

--- app/about/page.js
+++ app/about/page.js
@@ -14,12 +14,12 @@ const page = () => {
           <div className="overflow-hidden rounded-lg border bg-background shadow p-4">
             <p className="text-base text-gray-700">
               Recientemente me mude y tuve que habilitar una casa "desde 0".
-              Encontrar prestadores de servicios es difícil. En Google la
-              información esta desactualizada, no atienden, o no son de
+              Encontrar prestadores de servicios es dificil. En Google la
+              informacion esta desactualizada, no atienden, o no son de
               confianza.
             </p>
             <p className="mt-4 text-base text-gray-700">
-              Por eso cree este proyecto, dónde puedes encontrar servicios que
+              Por eso cree este proyecto, donde puedes encontrar servicios que
               han sido recomendados y curados directamente por tus vecinos.
               Espero sea de ayuda.
             </p>
